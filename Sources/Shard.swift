3f9cae5f1cc3adefb06b894e53af71d0db1162ab
Alejandro <aalonso128@outlook.com>
2016-12-27 00:42:57 -0600

    Parent references are now weak to prevent strong cycles
    Also changed public boolean property names to be more “swifty”

--- Sources/Shard.swift
+++ Sources/Shard.swift
@@ -13,45 +13,45 @@ import WebSockets
 class Shard {
 
   // MARK: Properties
-  
+
   /// ID of shard
   let id: Int
-  
+
   /// Amount of shards bot should be connected to
   let shardCount: Int
-  
+
   /// Session ID of gateway
   var sessionId: String?
-  
+
   /// Heartbeat worker
   var heartbeat: Heartbeat?
 
   /// Gateway URL for gateway
   var gatewayUrl = ""
-  
+
   /// WS
   var session: WebSocket?
-  
+
   /// Parent class
   let sword: Sword
-  
+
   /// Global Event Rate Limiter
   let globalBucket: Bucket
-  
+
   /// Presence Event Rate Limiter
   let presenceBucket: Bucket
 
   /// Whether or not the shard is connected to gateway
-  var connected: Bool = false
-  
+  var isConnected: Bool = false
+
   /// The last sequence sent by Discord
   var lastSeq: Int?
 
   // MARK: Initializer
-  
+
   /**
    Creates Shard Handler
-   
+
    - parameter sword: Parent class
    - parameter id: ID of the current shard
    - parameter shardCount: Total number of shards bot needs to be connected to
@@ -66,10 +66,10 @@ class Shard {
   }
 
   // MARK: Functions
-  
+
   /**
    Starts WS connection with Discord
-   
+
    - parameter gatewayUrl: URL that WS should connect to
   */
   func startWS(_ gatewayUrl: String, reconnect: Bool = false, reconnectPayload: String? = nil) {
@@ -77,7 +77,7 @@ class Shard {
 
     try? WebSocket.connect(to: gatewayUrl) { ws in
       self.session = ws
-      self.connected = true
+      self.isConnected = true
 
       if reconnect {
         self.send(reconnectPayload!)
@@ -88,7 +88,7 @@ class Shard {
       }
 
       ws.onClose = { ws, code, _, _ in
-        self.connected = false
+        self.isConnected = false
         switch CloseCode(rawValue: Int(code!))! {
           case .authenticationFailed:
             print("[Sword] - Invalid Bot Token")
@@ -103,12 +103,12 @@ class Shard {
 
   /**
    Used to reconnect to gateway
-   
+
    - parameter payload: Reconnect payload to send to connection
   */
   func reconnect(_ payload: Payload) {
     try? self.session!.close()
-    self.connected = false
+    self.isConnected = false
     self.heartbeat = nil
 
     self.startWS(self.gatewayUrl, reconnect: true, reconnectPayload: payload.encode())
@@ -117,13 +117,13 @@ class Shard {
   /// Used to stop WS connection
   func stop() {
     try? self.session!.close()
-    self.connected = false
+    self.isConnected = false
     self.heartbeat = nil
   }
 
   /**
    Sends a payload through WS connection
-   
+
    - parameter text: JSON text to send through WS connection
    - parameter presence: Whether or not this WS payload updates shard presence
   */
@@ -148,7 +148,7 @@ class Shard {
 
   /**
    Handles gateway events from WS connection with Discord
-   
+
    - parameter payload: Payload struct that Discord sent as JSON
   */
   func event(_ payload: Payload) {
--- Sources/User.swift
+++ Sources/User.swift
@@ -12,39 +12,39 @@ import Foundation
 public struct User {
 
   // MARK: Properties
-  
+
   /// Parent class
-  private let sword: Sword
+  private weak var sword: Sword?
 
   /// Avatar hash
   public let avatar: String?
-  
+
   /// Whether or not this user is a bot
-  public let bot: Bool?
-  
+  public let isBot: Bool?
+
   /// Discriminator of user
   public let discriminator: String?
-  
+
   /// Email of user (will probably be empty forever)
   public let email: String?
-  
+
   /// ID of user
   public let id: String
-  
+
   /// Whether of not user has mfa enabled (will probably be empty forever)
-  public let mfaEnabled: Bool?
-  
+  public let isMfaEnabled: Bool?
+
   /// Username of user
   public let username: String?
-  
+
   /// Whether user is verified or not
-  public let verified: Bool?
+  public let isVerified: Bool?
 
   // MARK: Initializer
-  
+
   /**
    Creates User struct
-   
+
    - parameter sword: Parent class to get properties from
    - parameter json: JSON to decode into User struct
   */
@@ -53,23 +53,23 @@ public struct User {
 
     self.id = json["id"] as! String
     self.avatar = json["avatar"] as? String
-    self.bot = json["bot"] as? Bool
+    self.isBot = json["bot"] as? Bool
     self.discriminator = json["discriminator"] as? String
     self.email = json["email"] as? String
-    self.mfaEnabled = json["mfaEnabled"] as? Bool
+    self.isMfaEnabled = json["mfaEnabled"] as? Bool
     self.username = json["username"] as? String
-    self.verified = json["verified"] as? Bool
+    self.isVerified = json["verified"] as? Bool
   }
 
   // MARK: Functions
-  
+
   /// Gets DM for user
   public func getDM(_ completion: @escaping (DMChannel?) -> ()) {
-    self.sword.requester.request(self.sword.endpoints.createDM(), body: ["recipient_id": self.id].createBody(), method: "POST") { error, data in
+    self.sword!.requester.request(self.sword!.endpoints.createDM(), body: ["recipient_id": self.id].createBody(), method: "POST") { error, data in
       if error != nil {
         completion(nil)
       }else {
-        completion(DMChannel(self.sword, data as! [String: Any]))
+        completion(DMChannel(self.sword!, data as! [String: Any]))
       }
     }
   }
