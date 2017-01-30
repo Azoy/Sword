e4e0e13a210af46b7f4b1230eff03d25895d9662
Alejandro <aalonso128@outlook.com>
2017-01-30 16:24:53 -0600

    get rid of unnecessary initializers for options

--- Sources/Options.swift
+++ Sources/Options.swift
@@ -7,74 +7,37 @@
 //
 
 /// Sword Options structure
-public struct SwordOptions {
+open struct SwordOptions {
 
   // MARK: Properties
 
   /// Whether or not caching offline members is allowed
-  public var isCacheAllMembers: Bool
+  open var isCacheAllMembers: Bool
 
   /// Array of event names to disable
-  public var disabledEvents: [Event]
+  open var disabledEvents: [Event]
 
   /// Whether or not this bot is sharded
-  public var isSharded: Bool
-
-  // MARK: Initializer
-
-  /**
-   Creates Sword Options structure
-
-   - parameter cacheAllMembers: Whether or not to cache offline members
-   - parameter disabledEvents: Array of event names to disable (improve performance)
-   - parameter sharded: Whether or not the bot should be sharded
-  */
-  public init(cacheAllMembers: Bool = false, disabledEvents: [Event] = [], sharded: Bool = true) {
-    self.isCacheAllMembers = cacheAllMembers
-    self.disabledEvents = disabledEvents
-    self.isSharded = sharded
-  }
+  open var isSharded: Bool
 
 }
 
 /// Shield Options structure
-public struct ShieldOptions {
+open struct ShieldOptions {
 
   // MARK: Properties
 
   /// Array of prefixes commands should start with
-  public var prefixes: [String]
-
-  // MARK: Initializer
-
-  /**
-   Creates a Shield Options structure
-
-   - parameter prefixes: Array of prefixes commands should start with
-  */
-  public init(prefixes: [String] = ["@bot"]) {
-    self.prefixes = prefixes
-  }
+  open var prefixes: [String]
 
 }
 
 /// Command Options structure
-public struct CommandOptions {
+open struct CommandOptions {
 
   // MARK: Properties
 
   /// Array of command aliases
-  public var aliases: [String]
-
-  // MARK: Initializer
-
-  /**
-   Creates Command Options structure
-
-   - parameter aliases: Array of command aliases
-  */
-  public init(aliases: [String] = []) {
-    self.aliases = aliases
-  }
+  open var aliases: [String]
 
 }
